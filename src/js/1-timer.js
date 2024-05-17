893a7168ce3e543c52530a1ef0a34e432ff18890
LigerCode3443 <161658640+LigerCode3443@users.noreply.github.com>
2024-05-17 15:36:07 +0300

    +

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,1 +1,27 @@
-console.log('hello timer');
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+flatpickr('#datetime-picker', options);
+
+//!===================================================================
+// iziToast.show({
+//   title: 'Hey',
+//   message: 'What would you like to add?',
+// });
+//!===================================================================
+
+// const inputEl = document.querySelector('#datetime-picker');
+
+// const btnStart = document.querySelector('data-start');
+// console.log(btnStart);
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,58 @@
-console.log('hello bar');
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import errorIcon from '../img/error.svg';
+// iziToast.show({
+//   title: 'Hey',
+//   message: 'What would you like to add?',
+// });
+
+const objOk = {
+  title: '✅',
+  message: ` Fulfilled promise in ()ms`,
+  backgroundColor: 'green',
+  timeout: 5000,
+  position: 'topCenter',
+};
+const objErro = {};
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', evt => {
+  evt.preventDefault();
+  const formData = new FormData(formEl);
+  const userData = {
+    delay: formData.get('delay'),
+    state: formData.get('state'),
+  };
+  createPromise(userData.delay, userData.state)
+    .then(() => {
+      setTimeout(() => {
+        return iziToast.show(objOk, userData.delay);
+      }, userData.delay);
+    })
+    .catch(() => {
+      setTimeout(() => {
+        return iziToast.error({
+          iconUrl: errorIcon,
+          message: ` Rejected promise in ${userData.delay}ms`,
+          backgroundColor: '#ef4040',
+          messageColor: '#fff',
+          timeout: 5000,
+          position: 'topLeft',
+        });
+      }, userData.delay);
+    });
+});
+
+function createPromise(delay, state) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve();
+      } else {
+        reject();
+      }
+    }, delay);
+  });
+  return promise;
+}
